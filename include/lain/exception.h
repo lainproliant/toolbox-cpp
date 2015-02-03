82649e6e753186cc6c301ab70a8caaa8b5d6dc1a
Lain Supe <lain.proliant@gmail.com>
2015-02-02 21:32:22 -0800

    Fixed bug with stack trace vector, added option to print stack trace in normal exception messages.

--- include/lain/exception.h
+++ include/lain/exception.h
@@ -10,18 +10,24 @@
 #include <vector>
 #include <ostream>
 
+#include "lain/string.h"
+#include "tinyformat/tinyformat.h"
+
 namespace lain {
    using namespace std;
 
-   vector<string> generate_stacktrace(int max_frames = 256) {
+   inline vector<string> generate_stacktrace(int max_frames = 256) {
 #ifdef LAIN_ENABLE_STACKTRACE
       void* frames[max_frames];
       char** formatted_frames;
       size_t num_frames = backtrace(frames, max_frames);
-      vector<string> btvec(num_frames);
+      vector<string> btvec;
       formatted_frames = backtrace_symbols(frames, num_frames);
       for (size_t x = 0; x < num_frames; x++) {
-         btvec.push_back(string(formatted_frames[x]));
+         string formatted_frame = string(formatted_frames[x]);
+         if (formatted_frame.size() > 0) {
+            btvec.push_back(formatted_frame);
+         }
       }
 #else
       vector<string> btvec(0);
@@ -29,19 +35,30 @@ namespace lain {
 
       return btvec;
    }
+
+   inline string format_stacktrace(const vector<string>& stacktrace) {
+      return tfm::format("Stack Trace -->\n\t%s",
+            str::join(stacktrace, "\n\t"));
+   }
+
    /**
     * A generic base exception class.
     */
    class Exception : public runtime_error {
    public:
-      Exception(const string& message) :
-         runtime_error(message.c_str()), message(message) {
+      Exception(const string& message) : runtime_error(message.c_str()) {
          stacktrace = generate_stacktrace();
+#ifdef LAIN_STACKTRACE_IN_DESCRIPTION
+         this->message = tfm::format("%s\n%s\n",
+            message, format_stacktrace(stacktrace));
+#else
+         this->message = message;
+#endif
       }
 
       virtual const char* what() const throw()
       {
-         return get_message().c_str();
+         return message.c_str();
       }
 
       virtual const string& get_message() const {
