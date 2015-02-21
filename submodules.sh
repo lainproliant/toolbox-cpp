8394458f87062e9e67aa73dbb9c92994048a4653
Lain Supe <lain.proliant@gmail.com>
2015-02-21 13:45:26 -0800

    Updated submodules.sh to do the right thing.

--- submodules.sh
+++ submodules.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-pushd include/picojson
-   git submodule init
-   git submodule update
-popd
-
-pushd include/tinyformat
-   git submodule init
-   git submodule update
-popd
-
+git submodule update --init --recursive
