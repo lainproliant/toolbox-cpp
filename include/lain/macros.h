d6300b765628fdb16d9c28698ca160df6e073f89
Lain Supe <lain.proliant@gmail.com>
2016-05-26 21:58:10 -0700

    Unswapp macros.h and maps.h (oops...)

--- include/lain/macros.h
+++ include/lain/macros.h
@@ -1,35 +1,6 @@
-#ifndef __LAIN_MAPS_H
-#define __LAIN_MAPS_H
+#ifndef __LAIN_MACROS_H
+#define __LAIN_MACROS_H
 
-#include <vector>
-
-namespace lain {
-   namespace maps {
-      using namespace std;
-
-      template<class T>
-      vector<typename T::key_type> keys(const T& map) {
-         vector<typename T::key_type> kvec;
-
-         for (auto iter = map.cbegin(); iter != map.cend(); iter++) {
-            kvec.push_back(iter->first);
-         }
-
-         return kvec;
-      }
-
-      template<class T>
-      vector<typename T::mapped_type> values(const T& map) {
-         vector<typename T::mapped_type> vvec;
-
-         for (auto iter = map.cbegin(); iter != map.cend(); iter++) {
-            vvec.push_back(iter->second);
-         }
-
-         return vvec;
-      }
-   }
-}
-
+#define UNUSED(x) ((void)(x))
 
 #endif
--- include/lain/maps.h
+++ include/lain/maps.h
@@ -1,6 +1,35 @@
-#ifndef __LAIN_MACROS_H
-#define __LAIN_MACROS_H
+#ifndef __LAIN_MAPS_H
+#define __LAIN_MAPS_H
 
-#define UNUSED(x) ((void)(x))
+#include <vector>
+
+namespace lain {
+   namespace maps {
+      using namespace std;
+
+      template<class T>
+      vector<typename T::key_type> keys(const T& map) {
+         vector<typename T::key_type> kvec;
+
+         for (auto iter = map.cbegin(); iter != map.cend(); iter++) {
+            kvec.push_back(iter->first);
+         }
+
+         return kvec;
+      }
+
+      template<class T>
+      vector<typename T::mapped_type> values(const T& map) {
+         vector<typename T::mapped_type> vvec;
+
+         for (auto iter = map.cbegin(); iter != map.cend(); iter++) {
+            vvec.push_back(iter->second);
+         }
+
+         return vvec;
+      }
+   }
+}
+
 
 #endif
